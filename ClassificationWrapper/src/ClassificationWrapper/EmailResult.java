411c8fa3b22bf3e3d0b2e4c4bba4683e67b68fa7
Vinit Shah <elysiums.shade@gmail.com>
2013-02-04 16:08:36 -0600

    Java project to handle NLP

---
+++ ClassificationWrapper/src/ClassificationWrapper/EmailResult.java
@@ -0,0 +1,28 @@
+package ClassificationWrapper;
+
+import java.io.File;
+
+import com.aliasi.classify.Classification;
+import com.aliasi.classify.Classified;
+import com.aliasi.classify.ConfusionMatrix;
+import com.aliasi.classify.JointClassification;
+import com.aliasi.classify.JointClassifier;
+import com.aliasi.classify.JointClassifierEvaluator;
+import com.aliasi.util.AbstractExternalizable;
+import com.aliasi.util.Files;
+
+public class EmailResult
+{
+	
+	// Do this so we can package as a runnable .jar
+	public static void main(String[] args)
+	{
+		// Do nothing, should not be called.
+	}
+	
+	public EmailResult() 
+	{
+
+	}
+
+}
